6bff9de40beb73785ab52c3d03c478f29906ed03
robst <46814454+rdjstamford@users.noreply.github.com>
2025-04-10 14:20:25 +0100

    created placeholders

--- src/main/java/uk/gov/hmcts/reform/dev/controllers/RootController.java
+++ src/main/java/uk/gov/hmcts/reform/dev/controllers/RootController.java
@@ -1,16 +1,33 @@
 package uk.gov.hmcts.reform.dev.controllers;
 
 import org.springframework.http.ResponseEntity;
+import static org.springframework.http.ResponseEntity.ok;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.http.ResponseEntity.ok;
-
 @RestController
+@RequestMapping("/")
 public class RootController {
 
     @GetMapping("/")
     public ResponseEntity<String> welcome() {
         return ok("Welcome to test-backend");
     }
+
+    @GetMapping("/create")
+    public ResponseEntity<String> create() {
+        return ok("Created.");
+        
+        //placeholder, create code to create entry
+    }
+
+    @DeleteMapping("/1")
+    public ResponseEntity<Object> delete() {
+        //placeholder, create code to delete entry
+        return ok("Deleted.");
+
+    }
+
 }
