18a0df0b44534b98f0221a0bf77698313b82d91b
robst <rdjstamford@gmail.com>
2025-04-10 14:11:17 +0100

    refactored name of ExampleCase to Case

--- src/main/java/uk/gov/hmcts/reform/dev/models/Case.java
+++ src/main/java/uk/gov/hmcts/reform/dev/models/Case.java
@@ -1,17 +1,17 @@
 package uk.gov.hmcts.reform.dev.models;
 
+import java.time.LocalDateTime;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
-
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
-public class ExampleCase {
+public class Case {
 
     private int id;
     private String caseNumber;
